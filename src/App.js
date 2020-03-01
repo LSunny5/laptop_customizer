a9d68c798985a8d55a7cadd1d6dbcc71e996d331
Sunny Lee <leesunny507@gmail.com>
2020-03-01 02:15:53 -0500

    feature side working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React, { Component } from 'react';
-
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-import slugify from 'slugify';
-
 import './App.css';
 
 //added by Sunny Lee
@@ -49,78 +44,20 @@ class App extends Component {
   };
 
   render() {
-
-
-
-
-
-
-
-
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
-
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
-
-    
-
-    
-
-
-
-
-
-
     return (
       <div className="App">
-        <Header /> 
+        <Header />
         <main>
-
-
-
-
-          
-        <form className="main__form">
-                <h2>Customize your laptop</h2>
-                {features}
-            </form>
-
-
-
-
-
-
-       
-        <Summary
-          USCurrencyFormat={USCurrencyFormat}
-          selected={this.state.selected}
-        />
+          <Feature
+            features={this.props.features}
+            selected={this.state.selected}
+            USCurrencyFormat={USCurrencyFormat}
+            updateFeature={this.updateFeature}
+          />
+          <Summary
+            USCurrencyFormat={USCurrencyFormat}
+            selected={this.state.selected}
+          />
         </main>
       </div>
     );
--- src/Feature/Feature.js
+++ src/Feature/Feature.js
@@ -1,25 +1,59 @@
 import React from 'react';
 import './Feature.css';
+import FeatureChoice from '../FeatureChoice/FeatureChoice';
+
+
+// Normalizes string as a slug - a string that is safe to use
+// in both URLs and html attributes
+import slugify from 'slugify';
 
 class Feature extends React.Component {
     render() {
+        const features = Object.keys(this.props.features).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const options = this.props.features[feature].map(item => {
+              const itemHash = slugify(JSON.stringify(item));
+              return (
+              
+              
+                <div key={itemHash} className="feature__item">
+                  <input
+                    type="radio"
+                    id={itemHash}
+                    className="feature__option"
+                    name={slugify(feature)}
+                    checked={item.name === this.props.selected[feature].name}
+                    onChange={e => this.props.updateFeature(feature, item)}
+                  />
+                  <label htmlFor={itemHash} className="feature__label">
+                    {item.name} ({this.props.USCurrencyFormat.format(item.cost)})
+                  </label>
+                </div>
 
+              );
+            });
+      
+
+
+
+
+            return (
+                <FeatureChoice 
+                    featureHash={featureHash}
+                    feature={feature}
+                    options={options}
+                    USCurrencyFormat={this.props.USCurrencyFormat}
+                />
+            );
+          });
 
         return (
             <form className="main__form">
                 <h2>Customize your laptop</h2>
-                
-
-
-
-                
+                {features}
             </form>
         )
-
-
     };
 }
-
-
 
 export default Feature
